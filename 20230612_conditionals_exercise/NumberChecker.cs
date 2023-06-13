ce0a589d8c5c697ecddc49de1833429254c937c1
Paul Winka <paul.winka@gmail.com>
2023-06-13 06:58:51 -0500

    init commit

---
+++ 20230612_conditionals_exercise/NumberChecker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _20230612_conditionals_exercise
+{
+    public class NumberChecker
+    {
+        public static bool CheckIfInteger(string response)
+        {
+            bool parseSuccessful;
+            int result;
+            return parseSuccessful = int.TryParse(response, out result);
+        }
+
+        public static string CheckIfInRange(int rangeNumber)
+        {
+            if (rangeNumber < 11 && rangeNumber > 0)
+            {
+                return "In range";
+            }
+            else
+            {
+                return "Not in range";
+            }
+        }
+
+        public static int ConvertToInt(string userInput)
+        {
+            int resultInteger = Int32.Parse(userInput);
+            return resultInteger;
+        }
+    }
+}
---
+++ 20230612_conditionals_exercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _20230612_conditionals_exercise
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number between 1 and 10.");
+            int validInteger;
+            var userInput = (Console.ReadLine());
+            bool isInteger = (NumberChecker.CheckIfInteger(userInput));
+            //Console.WriteLine(isInteger);
+            if (isInteger)
+            {
+                validInteger = NumberChecker.ConvertToInt(userInput);
+                //Console.WriteLine(validInteger);
+                string result = (NumberChecker.CheckIfInRange(validInteger));
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine("Not an integer.");
+            }
+        }
+    }
+}
