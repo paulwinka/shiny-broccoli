84fb3fd5bbfa5283f06ac9767eb62d9bc8bb25a1
Paul Winka <paul.winka@gmail.com>
2023-06-14 07:39:10 -0500

    init commit

---
+++ 20230614_control_flows/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _20230614_control_flows
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var rand = new Random();
+            for (var i = 0; i < 25; i++)
+            {
+                Console.Write((char)rand.Next(97, 122));
+            }
+            var password = new string()
+        }
+    }
+}
